4dbc30455de53e57ff491b60716ecffb6fcaf3cf
jhasakshi152 <jha.sakshi152@gmail.com>
2020-05-29 00:14:09 +0530

    Updates

---
+++ precache-manifest.ddf636c1aabcd2f4b8af8c81261913e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c6a46bd7a2a193a076140f12f7662c",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "1297c919e35f6dd278d1",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4f0b2d0ad26b9091589b",
+    "url": "/contactmanager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1297c919e35f6dd278d1",
+    "url": "/contactmanager/static/js/2.a443331d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.a443331d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0b2d0ad26b9091589b",
+    "url": "/contactmanager/static/js/main.a832639c.chunk.js"
+  },
+  {
+    "revision": "31aa565394677d9739a4",
+    "url": "/contactmanager/static/js/runtime-main.ab1f59d6.js"
+  }
+]);
